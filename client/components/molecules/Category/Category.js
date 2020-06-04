8f91e7ac183dd6126a82e96dbf6f73479a5df0c5
Amelie Oller <amelieoller@gmail.com>
2020-06-04 08:24:32 -0700

    Add category component

---
+++ client/components/molecules/Category/Category.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+
+import Trash from '_assets/icons/trash-2.svg';
+import Input from '_atoms/Input';
+import ColorDropdown from '../../molecules/ColorDropdown/ColorDropdown';
+import { attemptDeleteCategory, attemptUpdateCategory } from '_thunks/categories';
+import Save from '_assets/icons/save.svg';
+
+const Category = ({ category }) => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState(category.title);
+  const [color, setColor] = useState(category.color);
+  const [hasBeenModified, setHasBeenModified] = useState(false);
+
+  const deleteCategory = () => dispatch(attemptDeleteCategory(category.id));
+
+  const handleUpdateCategory = () => {
+    if (hasBeenModified) {
+      dispatch(attemptUpdateCategory({ id: category.id, title: title, color: color }));
+      setHasBeenModified(false);
+    }
+  };
+
+  return (
+    <StyledCategory key={category.id}>
+      <Input
+        label="Edit Category"
+        handleOnBlur={(newTitle) => {
+          setHasBeenModified(true);
+          setTitle(newTitle);
+        }}
+        defaultValue={title}
+      />
+      <ColorDropdown
+        onChange={(newColor) => {
+          setHasBeenModified(true);
+          setColor(newColor);
+        }}
+        currentColor={color}
+      />
+      <IconWrapper
+        onClick={() => {
+          if (window.confirm(`Are you sure you want to delete the board "${title}"?`))
+            deleteCategory();
+        }}
+        color="red"
+      >
+        <Trash />
+      </IconWrapper>
+
+      <IconWrapper
+        onClick={handleUpdateCategory}
+        color={hasBeenModified ? 'green' : 'grey'}
+      >
+        <Save />
+      </IconWrapper>
+    </StyledCategory>
+  );
+};
+
+const StyledCategory = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 3px;
+  position: relative;
+`;
+
+const IconWrapper = styled.button`
+  background: ${({ color }) => color};
+  border: none;
+  border-radius: ${({ theme }) => theme.sizes.borderRadius};
+  margin-left: ${({ theme }) => theme.sizes.spacingSmall};
+  display: flex;
+  align-items: center;
+
+  svg {
+    color: white;
+  }
+`;
+
+Category.propTypes = {
+  category: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    color: PropTypes.string,
+  }),
+};
+
+export default Category;
--- client/components/organisms/Categories/Categories.js
+++ client/components/organisms/Categories/Categories.js
@@ -1,55 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import * as R from 'ramda';
-import styled from 'styled-components';
 
-import {
-  attemptGetCategories,
-  attemptDeleteCategory,
-  attemptUpdateCategory,
-} from '_thunks/categories';
 import AddCategory from '_molecules/AddCategory';
-import Trash from '_assets/icons/trash-2.svg';
-import UpdateTextButton from '_molecules/UpdateTextButton';
+import Category from '_molecules/Category';
 
 const Categories = ({ boardId }) => {
-  const dispatch = useDispatch();
   const { categories } = useSelector(R.pick(['categories']));
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    boardId && dispatch(attemptGetCategories(boardId)).then(() => setLoading(false));
-  }, []);
-
-  const deleteCategory = (id) => dispatch(attemptDeleteCategory(id));
-
-  const handleUpdateCategory = (attribute, id) => {
-    dispatch(attemptUpdateCategory({ id: id, ...attribute }));
-  };
 
   return (
     <div>
-      {categories &&
-        categories.map((c) => (
-          <Category key={c.id}>
-            <UpdateTextButton
-              text={c.title}
-              handleUpdate={(newText) => handleUpdateCategory({ title: newText }, c.id)}
-            />
-            <Trash onClick={() => deleteCategory(c.id)} />
-          </Category>
-        ))}
+      {categories && categories.map((c) => <Category key={c.id} category={c} />)}
       <AddCategory boardId={boardId} />
     </div>
   );
 };
-
-const Category = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 3px;
-`;
 
 Categories.propTypes = {
   boardId: PropTypes.string.isRequired,
