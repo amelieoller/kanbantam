731671d8e471f6b25b8c5f9dbae96bd259dfb51d
Amelie Oller <amelieoller@gmail.com>
2020-06-14 12:58:19 -0700

    Update environment variables

--- server/config/environment.js
+++ server/config/environment.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
 process.env.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/mern';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'mern';
 process.env.PORT = process.env.PORT || 4000;
