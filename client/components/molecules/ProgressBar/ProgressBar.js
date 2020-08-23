63bbbb9d7644af2cbb0d3f05a92123fa45b774bc
Amelie Oller <amelieoller@gmail.com>
2020-08-23 15:54:15 -0700

    Move ProgressBar styles into separate file

--- client/components/molecules/ProgressBar/ProgressBar.js
+++ client/components/molecules/ProgressBar/ProgressBar.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 import MinusCircleIcon from '_assets/icons/minus-circle.svg';
 import PlusCircleIcon from '_assets/icons/plus-circle.svg';
 import Button from '_atoms/Button';
+import {
+  Wrapper,
+  ProgressBarWrapper,
+  ProgressWrapper,
+  ProgressFiller,
+  TextLeft,
+} from './ProgressBarStyles';
 
 const ProgressBar = ({
   total,
@@ -74,52 +80,6 @@ const ProgressBar = ({
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-`;
-
-const ProgressBarWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-
-  & > *:not(:last-child) {
-    margin-right: 3px;
-  }
-`;
-
-const ProgressWrapper = styled.div`
-  position: relative;
-  height: 12px;
-  border-radius: 10px;
-  background: ${({ theme }) => theme.colors.lighter(1, 'surfaceVariant')};
-  width: 100%;
-`;
-
-const ProgressFiller = styled.div`
-  background: ${({ theme, percentage }) =>
-    percentage < 40
-      ? theme.colors.lighter(75, 'onSurface')
-      : theme.colors.lighter(parseInt(1000 / percentage), 'success')};
-  height: 100%;
-  border-radius: inherit;
-  transition: width 0.2s ease-in;
-  display: flex;
-  align-items: center;
-  width: ${({ percentage }) => percentage}%;
-`;
-
-const TextLeft = styled.span`
-  color: ${({ theme, percentage }) =>
-    percentage < 40 ? theme.colors.lighter(2, 'onSurface') : theme.colors.onSurfaceVariant};
-  font-size: 0.75rem;
-  font-style: italic;
-  white-space: nowrap;
-  padding-left: 4px;
-`;
 
 ProgressBar.propTypes = {
   total: PropTypes.number.isRequired,
---
+++ client/components/molecules/ProgressBar/ProgressBarStyles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+export const ProgressBarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  & > *:not(:last-child) {
+    margin-right: 3px;
+  }
+`;
+
+export const ProgressWrapper = styled.div`
+  position: relative;
+  height: 12px;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.lighter(1, 'surfaceVariant')};
+  width: 100%;
+`;
+
+export const ProgressFiller = styled.div`
+  background: ${({ theme, percentage }) =>
+    percentage < 40
+      ? theme.colors.lighter(75, 'onSurface')
+      : theme.colors.lighter(parseInt(1000 / percentage), 'success')};
+  height: 100%;
+  border-radius: inherit;
+  transition: width 0.2s ease-in;
+  display: flex;
+  align-items: center;
+  width: ${({ percentage }) => percentage}%;
+`;
+
+export const TextLeft = styled.span`
+  color: ${({ theme, percentage }) =>
+    percentage < 40 ? theme.colors.lighter(2, 'onSurface') : theme.colors.onSurfaceVariant};
+  font-size: 0.75rem;
+  font-style: italic;
+  white-space: nowrap;
+  padding-left: 4px;
+`;
